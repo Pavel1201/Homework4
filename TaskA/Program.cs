e61600d7464fb83422b02ae1814e45c547ff3600
Pavel <p.a.dudarev@mail.ru>
2022-09-13 20:15:48 +0500

    Increasing

--- TaskA/Program.cs
+++ TaskA/Program.cs
@@ -8,13 +8,13 @@ for (int i = 0; i < 8; i++)
     
 }
 
-double arithmeticMean(int[] array1)
+double arithmeticMean(int[] array)
 {
     double Arithmet = 0;
     double sum = 0;
     for(int i = 0; i < 8; i++)
     {
-        sum = sum + array1[i];
+        sum = sum + array[i];
     }
     Arithmet = sum / 8;
     return Arithmet;
@@ -43,3 +43,59 @@ int maximum (int[] array)
 
 int maximum1 = maximum(array);
 Console.WriteLine(maximum1);
+
+void even(int[] array)
+{   int count = 0;
+    for(int i = 0; i < 8; i++)
+    {
+        if(array[i] % 2 != 0)
+        {
+            Console.WriteLine("Нет");
+            break;
+        }
+        else
+        {
+            count++;
+
+        }
+
+    }
+    if(count == 8)
+    {
+        Console.WriteLine("Все числа четные");
+
+    }
+
+
+
+}
+
+even(array);
+
+void increasing (int[] array)
+{   int count = 0;
+    for(int i = 1; i < 8; i++)
+    {
+        if(array[i] < array[i-1])
+        {
+            Console.WriteLine("Нет");
+            break;
+
+        }
+        else
+        {
+            count++;
+
+        }
+                             
+    }
+    if( count == 7)
+    {
+        Console.WriteLine("Массив по возрастанию.");
+
+    }
+
+
+}
+
+increasing(array);
