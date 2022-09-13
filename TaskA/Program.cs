86b6aa1928a0dda8256be5025302e2c61e4a388b
Pavel <p.a.dudarev@mail.ru>
2022-09-13 19:54:24 +0500

    Max

--- TaskA/Program.cs
+++ TaskA/Program.cs
@@ -5,6 +5,7 @@ int[] array = new int[8];
 for (int i = 0; i < 8; i++)
 {
     array[i] = new Random().Next(1,100);
+    
 }
 
 double arithmeticMean(int[] array1)
@@ -19,6 +20,26 @@ double arithmeticMean(int[] array1)
     return Arithmet;
 }
 
-Console.Write(arithmeticMean(array));
+Console.WriteLine(arithmeticMean(array));
+
+int maximum (int[] array)
+{
+    int max = array[0];
+    for (int i = 0; i < 8; i++)
+    {
+        if(array[i]  > max)
+        {
+            max = array[i];
 
 
+        }
+        
+
+
+    }
+    return max;
+
+}
+
+int maximum1 = maximum(array);
+Console.WriteLine(maximum1);
