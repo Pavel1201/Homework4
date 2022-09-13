236aa9f00399b5998af81e772e43418cf75386cc
Pavel <p.a.dudarev@mail.ru>
2022-09-13 16:38:01 +0500

    Add Task 25

---
+++ Task025/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число!");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите, в какую степень возвести!");
+int degree = int.Parse(Console.ReadLine()!);
+
+int exponentiation(int number1, int degree1)
+{
+    int product = 1;
+    for(int i = 0; i < degree1; i++)
+    {
+        product = product * number1;
+
+    }
+    return product;
+
+}
+
+Console.WriteLine(exponentiation(number, degree));
