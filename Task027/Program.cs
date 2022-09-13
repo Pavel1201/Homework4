5a6fead69209e432f38b4144582873edbb2737fe
Pavel <p.a.dudarev@mail.ru>
2022-09-13 16:48:47 +0500

    Add Task 27

---
+++ Task027/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число!");
+int number = int.Parse(Console.ReadLine()!);
+
+int sumofDigits(int number1)
+{
+    int sum = 0;
+    int digit = 0;
+
+    while(number1!=0)
+{
+    digit = number1 % 10;
+    sum = sum + digit;
+    number1 = number1 / 10;
+}
+return sum;
+}
+
+Console.WriteLine(sumofDigits(number));
